e27b40011513c4276cab57fb7d32560a90a23387
Ben Hilger <bhilger78@gmail.com>
2024-02-15 19:16:01 -0500

    add basic 5 part test

---
+++ README.md
@@ -0,0 +1,27 @@
+# PHP knowledge test
+
+This is a basic knowledge test for an intern-level position of their ability to pick up PHP (or their existing knowledge of PHP).
+
+## Setup environment for the test
+
+The bare minimum you need is a text editor/IDE (ideally that supports PHP syntax highlighting). But, if you want to test everything locally, you'll need to install PHP.
+
+To install PHP locally, please follow these [instructions](https://www.php.net/manual/en/install.php).
+
+Make sure you also clone (or fork) the repository locally. You'll want to setup your own repository where you can push the results.
+
+## About the test
+
+This is an open-note test (except AI assistants, you'd be robbing yourself of the chance to learn) to test how you would work being thrown into a new language and learning the basic.
+
+This test also tests your knowledge of using git, since it requires you to clone the repository locally, and then publish your results to your own repository. Remember, it's ok not to know right now, but hopefully you can figure out how to by the end of this test.
+
+This is broken into multiple parts, each one building on the previous. This isn't meant to test the advanced parts of PHP, but to see how quickly someone can pick up basic syntax.
+
+## Important Refernces
+
+Refer to the [documentation](https://www.php.net/manual) first. It'll be extremely valuable throughout this test.
+
+## Online Editor
+
+If you'd like to use an online editor designed for PHP, you can use [phponline](https://onlinephp.io/). Just copy the contents of the test.php file, and then once you're finished make sure you add all of your code back to the test.php file so it can be submitted to GitHub.
---
+++ src/test.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace BenHilger\PhpKnowledgeTest;
+
+/**
+ * PART 1
+ * 
+ * Welcome to the php knowledge test. The purpose of this test is to
+ * test how quickly you can pick up the basic syntax of PHP
+ * 
+ * In part 1, we will focus on basic arithmetic operation (addition, subtraction, multiplication, division, remainder)
+ * You will be performing the basic arithmetic operation, and then assigning the result to a variable
+ * 
+ * For example, in Java, you can add and assign the result of the operation as follows
+ * Integer result = 1 + 5;
+ * 
+ * However, PHP does it slightly differently. Part of this exercise is to see if you're able to figure out that difference
+ * 
+ * Remember, this is an open note quiz (please avoid AI-assistants, you'll be robbing yourself of the learning opportunity)
+ */
+
+// add 2 and 4
+
+// subtract 10 and 1
+
+// multiply 4 and 10
+
+// divide 5 and 2
+
+// get the remainder of 5 and 2
+
+/**
+ * PART 2
+ * 
+ * Next, we will move to basic function definitions building on what was completed it part 1.
+ * Each comment below will specify the 
+ * 
+ * IMPORTANT: For this excerise, make sure you explicitly specify the type (int, string, array, etc) of all parameters, and the return type
+ */
+
+// Create a function (add) to add two values together (a and b)
+
+// Create a function (subtract) to subtract two values (a and b)
+
+// Create a function (multiply) to multiply two values (a and b)
+
+// Create a function (divide) to divide two values (a and b)
+
+// Create a function (remainder) to get the remainder of two values (a and b)
+
+/**
+ * PART 3
+ *
+ * Next, we will expand upon the functions and create a basic class
+ * with functions within it
+ * 
+ * The name and structure of the class is specified in the comment below
+ * 
+ * IMPORTANT: For this excerise, make sure you explicitly specify the type of all parameters, and the return type
+ */
+
+// Create a class (MathOperations), and within it, create the following functions:
+    // double => takes one parameter, and returns the parameter doubled
+    // isEven => takes one parameter, returns true if even, false otherwise
+
+/**
+ * PART 4
+ * 
+ * Next, we will expand upon the class and use a couple of the functions. 
+ * We will also introduce arrays, and one array function (array_map)
+ * 
+ * IMPORTANT: For this excerise, make sure you explicitly specify the type of all parameters, and the return type
+ */
+
+// declare an array with the values in this order 1, 10, 56, 2, 1
+
+// use array_map to create a new array of booleans based on the isEven function in the MathOperations class
+// for instance an array with values 1, 10, 56, 2, 1 becomes
+
+// use array_map to create a new array of numbers based on the double function in the MathOperations class
+// for instance an array with values 1, 10, 56, 2, 1 becomes
+
+/**
+ * PART 5
+ * 
+ * Finally, we will introduce associative arrays (similar to Maps)
+ * 
+ * IMPORTANT: For this excerise, make sure you explicitly specify the type of all parameters, and the return type
+ */
+
+ // Create an associative array with the following structure
+ // Key        || Value
+ // PHP        => 8.2
+ // Apple      => Vision Pro
+ // California => San Francisco
